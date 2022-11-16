e101de58f829ff4ff6a3acf02388180088308236
RanaRabees <104692519+RanaRabees@users.noreply.github.com>
2022-11-16 17:19:34 +0500

    Add files via upload

---
+++ static/js/all.js
@@ -0,0 +1,28 @@
+function roundst() {
+    var roundst = document.getElementById('roundst').style.backgroundColor = "darkgoldenrod";
+    // console.log('<marquee behavior="" direction=""> RANA G</marquee>');
+  }
+  function roundmouse() {
+    var roundst = document.getElementById('roundst').style.backgroundColor = "chartreuse";
+  }
+  function roundmouselv() {
+    var roundst = document.getElementById('roundst').style.backgroundColor = "aqua";
+  }
+  function seclast1() {
+    var seclast1 = document.getElementById('seclast1').style.backgroundColor = "darkseagreen";
+  }
+  function seclast1mouse() {
+    var roundst = document.getElementById('roundst').style.backgroundColor = "crimson";
+  }
+  function seclast1lv() {
+    var roundst = document.getElementById('roundst').style.backgroundColor = "darkgoldenrod";
+  }
+  function seclast3() {  
+    var seclast3 = document.getElementById('seclast3').style.backgroundColor = "cadetblue";
+  }
+  function seclast3mouse() {
+    var roundst = document.getElementById('roundst').style.backgroundColor = "khaki";
+  }
+  function seclast3lv() {
+    var roundst = document.getElementById('roundst').style.backgroundColor = "darkseagreen";
+  }
